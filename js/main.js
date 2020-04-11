1164ed2156f5c65d6b371517f0fc22cf5a962af2
Travis Boatright <travis@Traviss-MacBook-Pro.local>
2020-04-11 14:24:55 -0400

    fixed data

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function formatDate(hours) {
 
 function formatTime(time) {
     let hour = time[0]
-    let minute = time[0]
+    let minute = time[1]
     let suffix = 'am';
     if (hour > 12) {
         suffix = 'pm';
